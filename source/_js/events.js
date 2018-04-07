061d54082e5f60089db37d0833293bb33959a56b
jaimelias <escribeme@jaimelias.com>
2018-04-06 21:11:46 -0500

    events.js

--- source/_js/events.js
+++ source/_js/events.js
@@ -14,6 +14,7 @@ function toggleExtras(event_id)
 	event_addons.classList.toggle('hidden');
 	return false;
 }
+
 function booking_submit(event_id)
 {
 	var event_form = document.getElementById("event-"+event_id);
@@ -54,5 +55,50 @@ function booking_submit(event_id)
 	}
 	
 	console.log(invalid);
+	console.log(output);
+}
+
+function calculate_total(event_id)
+{
+	var participants = 0;
+	var base_price = document.getElementById('event-json').innerHTML;
+	var event_form = document.getElementById('event-' + event_id);
+	var participants = document.getElementById('event-participants-' + event_id).value;
+	participants = participants != '--' ? parseInt(participants) : 0;
+	
+	try
+	{
+		base_price = JSON.parse(base_price);
+	}
+	catch(e)
+	{
+		console.log(e);
+	}
+
+	base_price = base_price ? parseFloat(base_price) : 0;
+	var output = base_price;	
+	var event_price = document.getElementById('event-json-' + event_id).innerHTML;
+	
+	try
+	{
+		event_price = JSON.parse(event_price);
+	}
+	catch(e)
+	{
+		console.log(e);
+	}
+	
+	event_price = event_price.price;
+	
+	if(Array.isArray(event_price))
+	{
+		//do nothing
+	}
+	else
+	{
+		//do  nothing
+		output = output + (event_price * participants);
+	}
+	
 	console.log(output);
 }
