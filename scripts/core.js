d03e1393ea8e6759fffb99e8c10f96dc208a4391
jaimelias <escribeme@jaimelias.com>
2018-04-28 18:16:32 -0500

    fix zoho

--- scripts/core.js
+++ scripts/core.js
@@ -6,21 +6,191 @@ var algoliasearch = require('algoliasearch');
 var client = algoliasearch("47Q1KIRCHF", "9a270bdc08cc20f4290e046efe0f98d3");
 var index = client.initIndex('mamba');
 var math = require('mathjs');
+var request = require('request');
+var https = require('https');
+var async = require('async');
+var Zoho = new zoho();
 
-hexo.extend.console.register('algolia', 'Clear Algolia Index', function(args){
-	
-	index.clearIndex(function(err, content) {
-	  console.log(content);
-	});
-	
-	var source = fs.readFileSync('db.json', {encoding: 'utf8'});
+function findObjectByKey(array, key, value) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i][key] == value) {
+            return array[i];
+        }
+    }
+    return null;
+}
+
+function zoho()
+{
+	var obj = {};
+	obj.app_name = 'mamba';
+	obj.url = 'https://creator.zoho.com/api/json/'+obj.app_name+'/view/';	
+	obj.views = {};
+	obj.views.packages = 'All_Packages';
+	obj.views.sub_packages = 'All_Sub_Packages';
+	obj.views.add_ons = 'All_Add_ons';
+	var params = {};
+	params.authtoken = 'e6abd5db9451b20d45cb15ada824ed97';
+	params.zc_ownername = 'panamajethub';
+	params.scope = 'creatorapi';
+	params.raw = true;
+	obj.params = Object.keys(params).map(key => key + '=' + params[key]).join('&');	
+	obj.fix_rel = function(string){
+		if(string != '[]')
+		{
+			return string.replace(/[\[\]']+/g,'').split(', ');
+		}
+		else
+		{
+			return [];
+		}
+	};
+	return obj;
+}
+
+hexo.on('generateBefore', function(){
+
+	var source = fs.readFileSync('zoho.json', {encoding: 'utf8'});
 	source = JSON.parse(source);
-	source = source.models.Page;
-	var output = [];
+	var pages = hexo.locals.get('pages').data;
+	pages = JSON.parse(JSON.stringify(pages));
 	
-	Object.keys(source).forEach(function(page){
+	for(var x = 0; x < source.length; x++)
+	{
+		pages.push(source[x]);
+	}
 		
-		var data = source[page];
+	hexo.locals.set('pages', function(){
+		return pages;
+	});	
+});
+
+hexo.extend.console.register('zoho', 'Clear Algolia Index', function(args){
+	zoho_run();
+});
+
+function zoho_run()
+{
+	var urls = [];
+	urls.push(Zoho.url + Zoho.views.packages+'?'+Zoho.params);
+	urls.push(Zoho.url + Zoho.views.sub_packages+'?'+Zoho.params);
+	urls.push(Zoho.url + Zoho.views.add_ons+'?'+Zoho.params);
+	
+	async.map(urls, function(url, cb) {
+	  https.get(url, function(res) {
+		var buf = '';
+		res.on('data', function(d) {
+		  buf += d;
+		}).on('end', function() {
+		  cb(null, buf);
+		});
+	  });
+	}, function(err, responses) {
+		zoho_packages(responses[0], responses[1], responses[2]);
+	});	
+}
+
+function zoho_packages(packages, sub_packages, addons)
+{	
+	var output = [];
+	packages = JSON.parse(packages);
+	packages = packages.Paquetes
+	sub_packages = JSON.parse(sub_packages);
+	sub_packages = sub_packages.Sub_Packages;
+	addons = JSON.parse(addons);
+	addons = addons.Add_ons;
+
+	for(var x = 0; x < packages.length; x++)
+	{
+		var obj = {};
+		obj.algolia = true;
+		obj.title = packages[x].title;
+		obj.magnetic_title = packages[x].magnetic_title;
+		
+		if(packages[x].magnetic_title == '')
+		{
+			obj.magnetic_title = packages[x].title;
+		}		
+		
+		obj.layout = 'page';
+		obj.date = '';
+		obj._id = packages[x].ID;
+		obj.path = string_to_slug(packages[x].title)+'/index.html';
+		obj.price = packages[x].price;
+		obj.description = packages[x].description;
+		obj._content = packages[x].content;
+		obj.events = [];
+		obj.type = Zoho.fix_rel(packages[x].Types);
+		obj.thumbnail = packages[x].thumbnail;
+			
+		for(var s = 0; s < Zoho.fix_rel(packages[x].Sub_Packages).length; s++)
+		{
+			var name = Zoho.fix_rel(packages[x].Sub_Packages)[s];
+			var sub = findObjectByKey(sub_packages, 'name', name);
+						
+			if(sub != null)
+			{
+				obj.events.push(sub);
+			}	
+		}
+		
+		for(var e = 0; e < obj.events.length; e++)
+		{
+			var name = Zoho.fix_rel(obj.events[e].Add_ons);
+			var add = findObjectByKey(addons, 'name', name);
+			obj.events[e].addons = [];
+			
+			var includes = '';
+			
+			if(typeof obj.events[e].includes === 'string')
+			{
+				if(obj.events[e].includes != '')
+				{
+					includes = obj.events[e].includes.split('\n');
+				}
+			}
+			
+			obj.events[e].includes = includes;
+				
+			if(add != null)
+			{
+				obj.events[e].addons.push(add);				
+			}
+		}
+		output.push(obj);
+	}
+	
+	if(output.length > 0)
+	{
+		if(output[0].algolia == true)
+		{
+			index.clearIndex(function(err, content) {
+			  console.log(content);
+			});	
+	
+			index.addObjects(filter_algolia(output), function(err, content) {
+				console.log(content.objectIDs.length + ' objects added to Algolia.');
+			});	
+
+			var path = 'zoho.json';
+			fs.writeFileSync(path, JSON.stringify(output), {encoding: 'utf8'});		
+		}
+	}
+	else
+	{
+		console.log('No Packages Found');
+	}
+}
+
+
+function filter_algolia(json)
+{
+	var output = [];
+
+	for(var x = 0; x < json.length; x++)
+	{
+		
+		var data = json[x];
 		
 		if(data.layout == 'page')
 		{
@@ -55,7 +225,7 @@ hexo.extend.console.register('algolia', 'Clear Algolia Index', function(args){
 						}
 					}
 				
-					if(Object.keys(data.events[0]).length > 0)
+					if(data.events.length > 0)
 					{
 						if(data.events[0].price)
 						{
@@ -78,21 +248,15 @@ hexo.extend.console.register('algolia', 'Clear Algolia Index', function(args){
 							console.log(data.title + 'has no price');
 						}
 					}
-			
-					
+	
 					output.push(obj);						
 				}			
 			}		
-		}
-	});
+		}		
+	}
 	
-	setTimeout(function(){
-		index.addObjects(output, function(err, content) {
-			console.log(content.objectIDs.length + ' objects added to Algolia.');
-		});										
-	}, 600);	
-	
-});
+	return output;
+}
 
 hexo.extend.helper.register('blaz', blaz_core);
 
@@ -268,4 +432,21 @@ function get_domain(url) {
     hostname = hostname.split(':')[0];
     hostname = hostname.split('?')[0];
     return hostname;
+}
+
+function string_to_slug (str) {
+    str = str.replace(/^\s+|\s+$/g, ''); // trim
+    str = str.toLowerCase();
+  
+    var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+    var to   = "aaaaeeeeiiiioooouuuunc------";
+    for (var i=0, l=from.length ; i<l ; i++) {
+        str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+    }
+
+    str = str.replace(/[^a-z0-9 -]/g, '')
+        .replace(/\s+/g, '-')
+        .replace(/-+/g, '-');
+
+    return str;
 }
